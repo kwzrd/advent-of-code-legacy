5e3094b29c72c91e524a95f36a2b7967ba3e6d0b
kwzrd <kwzrd@pythondiscord.com>
2020-12-01 10:25:04 +0100

    Day 01: implement solution

---
+++ aoc/solutions/day_01.py
@@ -0,0 +1,23 @@
+import itertools
+import math
+import typing as t
+
+
+def solve_for(numbers: t.List[int], n_factors: int) -> int:
+    """
+    Find the product of `numbers` which add up to 2020.
+
+    Give `n_factors` the amount of numbers to try to combine.
+    """
+    for combination in itertools.combinations(numbers, r=n_factors):
+        if sum(combination) == 2020:
+            return math.prod(combination)
+
+    raise Exception("No solution found")
+
+
+def main(input_data: str) -> t.Tuple[int, int]:
+    """Solution entry-point."""
+    numbers = [int(n) for n in input_data.split()]
+
+    return solve_for(numbers, n_factors=2), solve_for(numbers, n_factors=3)
